5c446d9ef1de1973c8082df23c77252b397b7b49
KernelTestPilot <oskardah@hotmail.com>
2022-12-15 15:21:28 +0100

    fungerade table book

--- js/main.js
+++ js/main.js
@@ -1,36 +1,18 @@
 
-let bookings = [];
+const bookings = [];
 // get data from the form
 function getData(form) {
     let formData = new FormData(form);
     let tablebooking = Object.fromEntries(formData);
-    console.log(tablebooking);
     let booking = new table(tablebooking.name, tablebooking.selectTable);
-    bookings.push(booking);
-    for(booking in bookings){
-        if(bookings.length > 1){
-        if(bookings[booking].tablenumber == tablebooking.selectTable){
-            console.log("finns redan")
-            bookings.splice(booking,3)
-        }else{
-            console.log("finns inte")
-        }
-    }
-    }
-    console.log(bookings);
+    if (bookings.some(e => e.tablenumber === tablebooking.selectTable)) {
+        bookings.pop();
+       console.log("finns redan")
+      }else{
+        console.log("finns inte")
+        bookings.push(booking);
+      }
   }
-
-
-
-
-
-
-
-
-
-
-
-
 
   //eventlistener for the tablebooking
   document.getElementById("tableform").addEventListener("submit", function (e) {
