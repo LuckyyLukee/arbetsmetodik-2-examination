cb81ccba0a72be3f975334897c904e14e19a564f
KernelTestPilot <oskardah@hotmail.com>
2022-12-17 11:40:35 +0100

    Testing version

--- js/products.js
+++ js/products.js
@@ -21,8 +21,37 @@ class products{
     }
     createCards(db,number,maincontainer){
         db[this.filter[number]].forEach(element => {
-            let {id, img, name, dsc, price, rate} = element;
+
+
+
+        let card = document.createElement("div");
+        card.classList.add("card")
+        
+        let {id, img, name, dsc, price, rate} = element;
+
+        card.setAttribute("id", id)
+
+
             console.log(element)
+
+            card.innerHTML = `
+    
+        <img
+            class="card-img"
+            src=${img}
+            alt=""
+        />
+        <div class="card-text">
+            <h2>${name}</h2>
+            <p>rating: ${rate}</p>
+            <p>
+            ${dsc}
+            </p>
+            <span class="card-price">${price} Sek</span>
+        </div>
+        <button class="add-to-cart">Add to cart</button>
+        `;
+        cardContainer.appendChild(card)
           });
     }
 }
