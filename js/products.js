76c229906b8cdaf445442c187609dcbb3f9d8878
KernelTestPilot <oskardah@hotmail.com>
2022-12-19 14:54:57 +0100

    fungerande

--- js/products.js
+++ js/products.js
@@ -19,13 +19,13 @@ class products{
             "our-foods"
         ]
     }
-
-    createCards(db,number,maincontainer){
-        db[this.filter[number]].forEach(element => {
+    createCards(number,maincontainer){
+        this.db[this.filter[number]].forEach(element => {
         let card = document.createElement("div");
         card.classList.add("card")
         let {id, img, name, dsc, price, rate} = element;
-        card.setAttribute("id", id)
+        card.setAttribute("id", name,)
+        card.setAttribute("price" , price)
             card.innerHTML = `
         <img
             class="card-img"
@@ -37,9 +37,6 @@ class products{
             <p>rating: ${rate}</p>
             <p>
             ${dsc}
-            <p>Rating: ${rate}</p>
-            <p>
-            ${dsc}.
             </p>
             <span class="card-price">${price} Sek</span>
         </div>
@@ -63,4 +60,5 @@ class products{
                 })
                }
             }
+
 }
