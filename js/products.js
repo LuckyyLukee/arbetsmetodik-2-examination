78220dc91593241fa5da42e13fd356baa750b323
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2022-12-19 19:41:47 +0100

    update products.js

--- js/products.js
+++ js/products.js
@@ -49,9 +49,6 @@ class products{
         for (let i = 0; i < btn.length; i++) {
             btn[i].addEventListener("click", function (e) {
             getProduct(e.target);
-            let orders = document.querySelector(".orders");
-            orders.style.visibility = "visible";
-            
             })
            }
         }
