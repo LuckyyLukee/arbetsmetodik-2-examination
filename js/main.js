7748ee8e519bc7bf08b81a7fb68fc106c06b93dd
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2022-12-19 15:24:44 +0100

    changes js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
+//random variabels
 const cardContainer = document.querySelector('.featured-grid')
+const  btn = document.getElementsByTagName('button');
+const tableform = document.querySelector('tableform')
+const listmenu = document.querySelectorAll('li');
+const order = document.querySelector('.orders')
 const bookings = [];
-// get data from the form
+//create object orderproducts
+let orderproducts = new products(db);
+//takes 2 parameters 1 for db one for drinks,bbq,exc
+
+//Create eventhandlers for btns and menu for products
+orderproducts.eventHandlerBtns(btn);
+orderproducts.eventHandlerMenu(listmenu);
+
+//remove orders
+//remove orders
+function removeOrder(e, x){
+
+  bookings[0].orders.splice(x, 1)
+      bookings[0].showOrders(order);
+}
+
+//to switch items in menu
+function createprodcuts(e){
+  document.querySelectorAll(".card").forEach(el => el.remove());
+  if(e.target.textContent == 'Food'){
+    orderproducts.createCards(8,cardContainer);
+}
+if(e.target.textContent == 'Snacks'){
+  orderproducts.createCards(2,cardContainer);
+}
+if(e.target.textContent == 'Drinks'){
+  orderproducts.createCards(6,cardContainer);
+}
+orderproducts.eventHandlerBtns(btn);
+orderproducts.eventHandlerMenu(listmenu);
+}
+//Get the orders, do methods in table class
+function getProduct(product){
+  bookings[0].getOrders(product.parentNode.getAttribute('id'), product.parentNode.getAttribute('price'));
+  bookings[0].showOrders(order)
+ } 
+
+// get data from the form, create a object table in bookings array
 function getData(form) {
     let formData = new FormData(form);
     let tablebooking = Object.fromEntries(formData);
@@ -12,15 +54,13 @@ function getData(form) {
         console.log("finns inte")
         bookings.push(booking);
       }
-      console.log(bookings)
   }
-  //create object products
-  let product = new products(db);
-  //takes 2 parameters 1 for db one for drinks,bbq,exc
-    product.createCards(db,6,cardContainer);
+
 
   //eventlistener for the tablebooking
   document.getElementById("tableform").addEventListener("submit", function (e) {
     e.preventDefault();
     getData(e.target);
   })
+
+
