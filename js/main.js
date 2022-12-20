16940f95bf990e22dacb28280260706c96d3c0d7
Lucas Magnusson <lucasmyeet@gmail.com>
2022-12-20 14:39:20 +0100

    fixed

--- js/main.js
+++ js/main.js
@@ -74,5 +74,8 @@ function styleCart() {
   })
 
 function chooseLanguage(language) {
-  
+  const nav = document.querySelector('.nav-list');
+  const languagePage = document.querySelector('.language-container');
+  languagePage.style.visibility = "hidden";
+  languagePage.style.zIndex = "29";
 }
