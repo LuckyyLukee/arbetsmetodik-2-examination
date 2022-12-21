b592ef1003ad455d2a6e225d889a14413df0ea7c
KernelTestPilot <oskardah@hotmail.com>
2022-12-21 20:25:55 +0100

    fixed code

--- js/search.js
+++ js/search.js
@@ -1,29 +1,25 @@
-const food = document.getElementsByClassName('card');
 const searchFoods = [];
 const input = document.querySelector(".search-bar");
 let query;
-console.log(food)
 input.addEventListener("keydown", (e) => {
-    if (input.value != "") {
+  if (input.value != "") {
     query = input.value;
     search(e);
-    }
+  }
 })
 
-function search(e){
-for (var i = 0; i < food.length; i++) {
-searchFoods.push({name: food[i].id.toLocaleLowerCase()});
-}
-  let search = searchFoods.filter(option => option.name.startsWith(query.toLowerCase()));
-  for(var a = 0; a < search.length; a++){
-    for (var i = 0; i < food.length; i++){  
-          if(search[a].name != food[i].id.toLocaleLowerCase()){
-        cardContainer.appendChild(food[i]);
-          }else{
-           
-          }
-      }
+function search(e) {
+  for (var i = 0; i < food.length; i++) {
+    searchFoods.push({ name: food[i].id.toLocaleLowerCase() });
   }
-  search.splice(0,search.length)
+  let search = searchFoods.filter(option => option.name.startsWith(query.toLowerCase()));
+  for (var a = 0; a < search.length; a++) {
+    for (var i = 0; i < food.length; i++) {
+      if (search[a].name != food[i].id.toLocaleLowerCase()) {
+        cardContainer.appendChild(food[i]);
+      }
+    }
+  }
+  search.splice(0, search.length)
   searchFoods.splice(0, searchFoods.length)
 }
--- js/tables.js
+++ js/tables.js
@@ -1,43 +1,40 @@
-class table{
-    constructor(name, tablenumber){
+class table {
+    constructor(name, tablenumber) {
         this.name = name;
         this.tablenumber = tablenumber;
         this.orders = [];
         this.sum;
     }
-    getTable(){
+    getTable() {
         console.log(this.name, this.tablenumber)
         return this.name + this.tablenumber;
     }
-    
-    getOrders(products,price){
+    getOrders(products, price) {
         const product = [products, parseInt(price)]
         this.orders.push(product);
-    } 
-    showOrders(maincontainer){
-            document.querySelectorAll(".info").forEach(el => el.remove());
-            for(let i = 0; i < this.orders.length; i++){
-                let info = this.createDom();
-                info[1].addEventListener("click", function (e) {
-                        removeOrder(e, i);     
-                })
-                info[0].innerHTML = this.orders[i]
-                maincontainer.append(info[0]);
-                info[0].append(info[1])
-                this.totalSum();
-            }
     }
-    showName(){
+    showOrders(maincontainer) {
+        document.querySelectorAll(".info").forEach(el => el.remove());
+        for (let i = 0; i < this.orders.length; i++) {
+            let info = this.createDom();
+            info[1].addEventListener('click', e => this.removeOrder(e, i));
+            info[0].innerHTML = this.orders[i]
+            maincontainer.append(info[0]);
+            info[0].append(info[1])
+            this.totalSum();
+        }
+    }
+    showName() {
         let info = this.createDom();
         let name = info[2].innerhtml = this.name;
         return name;
     }
-    showTable(){
+    showTable() {
         let info = this.createDom();
         let table = info[3].innerhtml = this.tablenumber;
         return table;
     }
-    createDom(){
+    createDom() {
         let btn = document.createElement("BUTTON");
         btn.className = 'btninfo'
         btn.innerHTML = "X";
@@ -50,17 +47,31 @@ class table{
         return [info, btn, bookingName, bookingTable];
     }
 
-    totalSum(){
-        var sum  = 0;
+    totalSum() {
+        var sum = 0;
         for (const item of this.orders) {
             sum = sum += item[1];
-          }
-          this.sum = calcMoms(sum,1.25);
-          sumTotal.innerText =  calcMoms(sum,1.25) + " :-";
+        }
+        this.sum = this.calcMoms(sum, 1.25);
+        sumTotal.innerText = this.calcMoms(sum, 1.25) + " :-";
     }
-    sendOrder(){
+    sendOrder() {
         this.orders.splice(0, this.orders.length)
         orders.style.visibility = 'hidden';
         return this.sum;
     }
+    removeOrder(e, x) {
+        this.orders.splice(x, 1)
+        this.showOrders(order);
+        if (this.orders.length == 0) {
+            sumTotal.innerText = '';
+            orders.style.visibility = 'hidden';
+        }
+    }
+    calcMoms(sum, moms) {
+        let sumWithMoms = sum * moms;
+        return sumWithMoms
+      }
+      
+
 }
